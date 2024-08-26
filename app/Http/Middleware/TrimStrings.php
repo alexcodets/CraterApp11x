9e9fd89fe33acc9e1f97e21a9bf03d9bd562a94d
Reny Ramos <renyramosgarcia@gmail.com>
2024-08-26 12:43:21 -0400

    Using the default and the newest value (from laravel 11)

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -6,13 +6,5 @@ use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
 
 class TrimStrings extends Middleware
 {
-    /**
-     * The names of the attributes that should not be trimmed.
-     *
-     * @var array
-     */
-    protected $except = [
-        'password',
-        'password_confirmation',
-    ];
+
 }
